53558ffefc240b02c145cbbc89b5e3a2b3c35dab
VemmyRM <kwvembu@gmail.com>
2021-04-25 12:31:20 -0400

    Adding server files

---
+++ server/index.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+var cors = require("cors");
+// // const pino = require("express-pino-logger")();
+// // const client = require("twilio")(
+// //   process.env.TWILIO_ACCOUNT_SID,
+// //   process.env.TWILIO_AUTH_TOKEN
+// // );
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors());
+
+var twilio = require("twilio");
+
+var accountSid = "ACda803fe28e3b582cf3b545e7cf6c7f55"; // Your Account SID from www.twilio.com/console
+var authToken = "4ec41599c38745ccb11941e45bacb5a5"; // Your Auth Token from www.twilio.com/console
+var client = new twilio(accountSid, authToken);
+
+console.log("hello!");
+
+// app.use(pino);
+
+// app.get("/api/greeting", (req, res) => {
+//   const name = req.query.name || "World";
+
+//   res.setHeader("Content-Type", "application/json");
+//   res.send(JSON.stringify({ greeting: `Hello ${name}!` }));
+// });
+
+app.post("/api/messages", (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Max-Age", "1800");
+  res.setHeader("Access-Control-Allow-Headers", "content-type");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "PUT, POST, GET, DELETE, PATCH, OPTIONS"
+  );
+  res.header("Content-Type", "application/json");
+  console.log(req.body);
+  client.messages
+    .create({
+      body: req.body.text,
+      to: req.body.to, // Text this number
+      from: "+17066617245", // From a valid Twilio number
+    })
+    .then((message) => {
+      res.send(JSON.stringify({ success: true }));
+    })
+    .catch((err) => {
+      console.log(err);
+      res.send(JSON.stringify({ success: false }));
+    });
+});
+
+app.listen(3001, () =>
+  console.log("Express server is running on localhost:3001")
+);
